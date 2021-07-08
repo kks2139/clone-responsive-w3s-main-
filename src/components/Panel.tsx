1a5576a3ffdf8c3144c247d93977ca05579de059
kwangsun <eyeyel@naver.com>
2021-07-08 23:13:30 +0900

    ui mod

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 function Panel(){
     return (
         <div className='panel'>
-            
+            <div className='intro-block'>
+                
+            </div>
+            <div className='code-block'>
+
+            </div>
         </div>
     );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -10,3 +10,4 @@ function Main(){
 }
 
 export default Main;
+
